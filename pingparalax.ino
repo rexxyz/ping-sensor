00bd3159fee137a01b06adaf585e4216417f59d7
rexxyz <73003602+rexxyz@users.noreply.github.com>
2020-10-17 07:21:57 +0700

    Update pingparalax.ino

--- pingparalax.ino
+++ pingparalax.ino
@@ -30,7 +30,10 @@ int dataAnalog2 = 2;
 int dataAnalog3 = 3;
 int dataAnalog4 = 4;
 
-
+// Create a new instance of the SharpIR class:
+SharpIR mySensor0 = SharpIR(Infrader_depan, model);
+SharpIR mySensor1 = SharpIR(Infrader_kanan, model);
+SharpIR mySensor2 = SharpIR(Infrader_kiri, model);
 
 NewPing sonar1(ping_depan, ping_depan, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 NewPing sonar2(ping_kiri_Depan, ping_kiri_Depan, MAX_DISTANCE);
